f5c273d73da0de08dbf835723c3502e01c096e4b
stark <serj.elessar@gmail.com>
2024-12-15 00:43:48 +0300

    Updates

--- assets/options-item-B-v-GoPQ.js
+++
@@ -1,5 +0,0 @@
-import{F as n}from"./index-C5coH1WH.js";const c=({value:t,onClick:o,isActive:i})=>{const s=document.createElement("li");return s.className=`bus-iti__options-item ${i?"bus-iti__options-item_active":""}`,s.dataset.phonemaskCountryCode=t.code,s.title=t.country,s.innerHTML=`
-		<span class="bus-iti__options-flag" style="background-position: ${n[t.code]}"></span>
-		<span class="bus-iti__options-name">${t.country}</span>
-		<span class="bus-iti__options-code">${t.prefix}</span>
-	`,s.onclick=()=>o(t),s};export{c as getOptionsItem};
