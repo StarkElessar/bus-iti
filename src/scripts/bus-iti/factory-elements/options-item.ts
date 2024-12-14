603060199529496513561a53474d3ca16df12177
stark <serj.elessar@gmail.com>
2024-12-14 16:19:03 +0300

    fix: update options item

--- src/scripts/bus-iti/factory-elements/options-item.ts
+++ src/scripts/bus-iti/factory-elements/options-item.ts
@@ -1,28 +1,21 @@
 import { type MaskValue } from '../types';
 
-type OptionsItemCallback = (data: MaskValue) => void;
+interface OptionsItemRenderProps {
+	value: MaskValue;
+	onClick: (data: MaskValue) => void;
+}
 
-export class OptionsItem {
-	private _selectMaskEvent: OptionsItemCallback[] = [];
+export const getOptionsItem = ({ value, onClick }: OptionsItemRenderProps) => {
+	const item = document.createElement('li');
 
-	render(value: MaskValue) {
-		const item = document.createElement('li');
-
-		item.classList.add('bus-iti__options-item');
-		item.dataset.phonemaskCountryCode = value.code;
-		item.innerHTML = `
+	item.classList.add('bus-iti__options-item');
+	item.dataset.phonemaskCountryCode = value.code;
+	item.innerHTML = `
 			<span class="bus-iti__options-flag bus-iti__options-flag_${value.code}"></span>
 			<span class="bus-iti__options-name">${value.country}</span>
 			<span class="bus-iti__options-code">${value.prefix}</span>
 		`;
-		item.onclick = () => {
-			this._selectMaskEvent.forEach(callback => callback(value));
-		};
+	item.onclick = () => onClick(value);
 
-		return item;
-	}
-
-	public onClick(callback: OptionsItemCallback) {
-		this._selectMaskEvent.push(callback);
-	}
-}
+	return item;
+};
