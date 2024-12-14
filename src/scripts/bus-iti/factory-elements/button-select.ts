01ab8099c5c9abd70f28d18226b8319737af6ef1
stark <serj.elessar@gmail.com>
2024-12-14 16:18:53 +0300

    fix: update button select

--- src/scripts/bus-iti/factory-elements/button-select.ts
+++ src/scripts/bus-iti/factory-elements/button-select.ts
@@ -1,22 +1,24 @@
-import { Country } from '../types';
+import type { MaskValue } from '../types';
 
 type ButtonSelectCallback = () => void;
 
 export class ButtonSelect {
 	private readonly _button: HTMLButtonElement;
 	private readonly _flagElement: HTMLSpanElement;
-	private _currentCode: Country;
+	private readonly _prefixElement: HTMLSpanElement;
+	private _currentValue: MaskValue;
 	private _toggleVisibleOptionsEvent: ButtonSelectCallback[] = [];
 
-	constructor(initialCode: Country) {
-		this._currentCode = initialCode;
+	constructor(value: MaskValue) {
+		this._currentValue = value;
 		this._button = document.createElement('button');
 
 		this._button.type = 'button';
 		this._button.classList.add('bus-iti__select');
 		this._button.innerHTML = `
-			<span class="bus-iti__select-flag" data-phonemask-flag="${this._currentCode}"></span>
+			<span class="bus-iti__select-flag" data-phonemask-flag="${this._currentValue.code}"></span>
 			<span class="bus-iti__select-arrow"></span>
+			<span class="bus-iti__select-prefix">${this._currentValue.prefix}</span>
 		`;
 
 		this._button.onclick = () => {
@@ -24,16 +26,18 @@ export class ButtonSelect {
 		};
 
 		this._flagElement = this._button.querySelector('.bus-iti__select-flag')!;
+		this._prefixElement = this._button.querySelector('.bus-iti__select-prefix')!;
 	}
 
 	public get buttonSelect() {
 		return this._button;
 	}
 
-	public set currentCode(value: Country) {
-		if (this._currentCode !== value) {
-			this._currentCode = value;
-			this._flagElement.setAttribute('data-phonemask-flag', value);
+	public set currentValue(value: MaskValue) {
+		if (this._currentValue.code !== value.code) {
+			this._currentValue = value;
+			this._flagElement.setAttribute('data-phonemask-flag', value.code);
+			this._prefixElement.textContent = value.prefix;
 		}
 	}
 
