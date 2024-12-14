9288eb4e89ea9821cf07112bd60ddf2ace57d2db
stark <serj.elessar@gmail.com>
2024-12-15 00:31:56 +0300

    feat: add search component

---
+++ src/scripts/bus-iti/factory-elements/search-input.ts
@@ -0,0 +1,40 @@
+import type { MaskItem, MaskListMap } from '../types';
+
+type OnSearchCallback = (data: MaskItem[]) => void;
+
+export class SearchInput {
+	private readonly _searchElement: HTMLInputElement;
+	private readonly _maskDataList: MaskItem[];
+	private _searchEvents: OnSearchCallback[] = [];
+
+	constructor(maskData: MaskListMap) {
+		this._maskDataList = Object.values(maskData);
+		this._searchElement = document.createElement('input');
+
+		this._searchElement.type = 'text';
+		this._searchElement.placeholder = 'Страна или код страны'
+		this._searchElement.className = 'bus-iti__options-search';
+
+		this._searchElement.oninput = (event) => {
+			const target = event.target as HTMLInputElement;
+			const value = target.value.toLowerCase().trim();
+
+			const data = this.maskDataList.filter(({ prefix, country }) =>
+				country.toLowerCase().includes(value) || prefix.includes(value));
+
+			this._searchEvents.forEach(callback => callback(data));
+		};
+	}
+
+	public onSearch(callback: OnSearchCallback) {
+		this._searchEvents.push(callback);
+	}
+
+	public get searchElement() {
+		return this._searchElement;
+	}
+
+	public get maskDataList(): MaskItem[] {
+		return this._maskDataList;
+	}
+}
