66eb6b6fa88e96a2633ca3d63b80e05a7f795c3f
stark <serj.elessar@gmail.com>
2024-12-16 21:19:44 +0300

    Updates

---
+++ assets/options-item-en1-bORA.js
@@ -0,0 +1,5 @@
+import{F as i}from"./index-QCWmo2dz.js";const e=({value:n,onClick:t,isActive:s})=>{const o=document.createElement("li");return o.className=`ipn__options-item ${s?"ipn__options-item_active":""}`,o.dataset.phonemaskCountryCode=n.code,o.title=n.country,o.innerHTML=`
+		<span class="ipn__options-flag" style="background-position: ${i[n.code]}"></span>
+		<span class="ipn__options-name">${n.country}</span>
+		<span class="ipn__options-code">${n.prefix}</span>
+	`,o.onclick=()=>t(n),o};export{e as getOptionsItem};
